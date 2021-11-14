d08332b4d655817ae223666ce087ea1842d78a09
jhpeng <alanpeng1995@gmail.com>
2021-11-14 10:05:05 +0800

    make it easiler to swatch

--- analysis/compare.py
+++ analysis/compare.py
@@ -2,27 +2,24 @@
 
 import numpy as np
 
-cluster_mag = np.loadtxt("../cluster_method/magz_l_16_beta_0.250000_t_8.txt")
-markovc_mag = np.loadtxt("../mc_sampling/magz_l_16_beta_0.250000_t_8.txt")
+cluster_mag = np.loadtxt("../cluster_method/magz_l_32_beta_0.200000_t_40.txt")
+markovc_mag = np.loadtxt("../mc_sampling/magz_l_32_beta_0.200000_t_40.txt")
 
 
+cl_mean = np.mean(cluster_mag[:,0])
+cl_err = np.std(cluster_mag[:,0])/np.sqrt(np.shape(cluster_mag)[0])
 
-nblock = np.shape(cluster_mag)[0]
-nT = np.shape(cluster_mag)[1]
-
-mean = np.mean(cluster_mag,axis=0)
-std = np.std(cluster_mag,axis=0)
-for t in range(nT):
-    print("T=%d %.6e +- %.6e"%(t,mean[t],std[t]/np.sqrt(nblock)))
-
-print("-------------------------------------")
+print("T=%d %.6e +- %.6e"%(0,cl_mean,cl_err))
 
 
-nblock = np.shape(markovc_mag)[0]
 nT = np.shape(markovc_mag)[1]
-
-mean = np.mean(markovc_mag,axis=0)
-std = np.std(markovc_mag,axis=0)
 for t in range(nT):
-    print("T=%d %.6e +- %.6e"%(t+1,mean[t],std[t]/np.sqrt(nblock)))
+    mk_mean = np.mean(markovc_mag[:,t])
+    mk_err = np.std(markovc_mag[:,t])/np.sqrt(np.shape(markovc_mag)[0])
+
+    cl_mean = np.mean(cluster_mag[:,t+1])
+    cl_err = np.std(cluster_mag[:,t+1])/np.sqrt(np.shape(cluster_mag)[0])
+
+    print("T=%d %.6e +- %.6e         %.6e +- %.6e "%(t,cl_mean,cl_err,mk_mean,mk_err))
+
 
