bd929fce38bba4464464b179833929bc94a77876
jhpeng <alanpeng1995@gmail.com>
2021-12-23 04:47:23 +0800

    create working space of random graph model

---
+++ random_graph/cluster_sampling.c
@@ -0,0 +1,349 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <gsl/gsl_rng.h>
+
+#include "union_find.h"
+
+int* Sigma;
+int* Bond2index;
+int Nsite,Nbond;
+
+int Nfix=0;
+double IAM=0;
+double Prob[16];
+double LocalWeight[16];
+
+int* Ptree;
+int* Weight;
+int* Flist;
+
+gsl_rng* rng;
+
+int bits2int(int *b) {
+    int i=0;
+    i+=b[0];
+    i+=(b[1])<<1;
+    i+=(b[2])<<2;
+    i+=(b[3])<<3;
+
+    return i;
+}
+
+void int2bits(int i, int* b) {
+    b[0] = i&1;
+    i = i>>1;
+    b[1] = i&1;
+    i = i>>1;
+    b[2] = i&1;
+    i = i>>1;
+    b[3] = i&1;
+}
+
+double configuration2weight(int i, double beta) {
+    int b[4],s;
+
+    int2bits(i,b);
+
+    s = b[1]+b[2]+b[3];
+    s = s*2-3;
+    double w = exp((b[0]*2-1)*s*beta);
+
+    return w/(w+1.0/w);
+}
+
+void initial_state(int L, int T, int type) {
+    if(type==0){
+        for(int i=0;i<L;i++) Sigma[i]=i%2;
+        for(int i=L;i<L*T;i++) {
+            Sigma[i]=1;
+            if(gsl_rng_uniform_pos(rng)<0.5){
+                Sigma[i]=0;
+            }
+        }
+    } else if(type==1) {
+        for(int i=0;i<L;i++) Sigma[i]=1;
+        for(int i=L;i<L*T;i++) {
+            Sigma[i]=1;
+            if(gsl_rng_uniform_pos(rng)<0.5){
+                Sigma[i]=0;
+            }
+        }
+    } else if(type==2) {
+        for(int i=0;i<L;i++) {
+            Sigma[i]=1;
+            if(i%4==0) Sigma[i]=0;
+        }
+        for(int i=L;i<L*T;i++) {
+            Sigma[i]=1;
+            if(gsl_rng_uniform_pos(rng)<0.5){
+                Sigma[i]=0;
+            }
+        }
+    } else if(type==3) {
+        for(int i=0;i<L*T;i++) {
+            Sigma[i]=1;
+            if(gsl_rng_uniform_pos(rng)<0.5){
+                Sigma[i]=0;
+            }
+        }
+    }
+}
+
+void initial_tree() {
+    for(int i=0;i<Nsite;i++) {
+        Ptree[i]  = i;
+        Weight[i] = 1;
+    }
+}
+
+
+void boundary_condition(int L, int T, int type) {
+    if(type==0) {
+        // fix the initial state
+        // free the final state
+        for(int i=0;i<L;i++) {
+            Weight[i] = -1;
+        }
+    } else if(type==1) {
+        // fix the initial state
+        // free the final state
+        for(int i=0;i<L;i++) {
+            Weight[i] = -1;
+        }
+        
+        for(int i=(T-1)*L; i<Nsite; i++) {
+            Weight[i] = -1;
+        }
+    } else if(type==2) {
+        for(int i=0;i<L;i++) {
+            if(Sigma[i]==1 && gsl_rng_uniform_pos(rng)<IAM) {
+                Weight[i] = -1;
+            }
+        }
+    } else if(type==3) {
+        // fix first n spin on up-state in the initial state and final state
+        for(int i=0;i<Nfix;i++) {
+            if(Sigma[i]==1)
+                Weight[i] = -1;
+        }
+        
+        for(int i=(T-1)*L; i<(T-1)*L+Nfix; i++) {
+            if(Sigma[i]==1)
+                Weight[i] = -1;
+        }
+    } else if(type==4) {
+        // fix first n spin on up-state in the final state
+        for(int i=0;i<L;i++) {
+            if(Sigma[i]==1 && gsl_rng_uniform_pos(rng)<IAM) {
+                Weight[i] = -1;
+            }
+        }
+
+        for(int i=(T-1)*L; i<(T-1)*L+Nfix; i++) {
+            if(Sigma[i]==1)
+                Weight[i] = -1;
+        }
+    }
+}
+
+void bond2index(int L, int T) {
+    if(Bond2index==NULL) {
+        Bond2index = (int*)malloc(sizeof(int)*(T-1)*L*4);
+    } else {
+        free(Bond2index);
+        Bond2index = (int*)malloc(sizeof(int)*(T-1)*L*4);
+    }
+
+    int ib=0;
+    for(int t=1;t<T;t++) {
+        for(int x=0;x<L;x++) {
+            Bond2index[4*ib+0] = x+t*L;
+            Bond2index[4*ib+1] = (x-1+L)%L + (t-1)*L;
+            Bond2index[4*ib+2] = x + (t-1)*L;
+            Bond2index[4*ib+3] = (x+1)%L + (t-1)*L;
+            ib++;
+        }
+    }
+
+    Nbond = ib;
+    Nsite = L*T;
+}
+
+double WeightOfConf;
+void clustering() {
+    int s[4];
+    int index[4];
+    WeightOfConf=0;
+    for(int ib=0;ib<Nbond;ib++) {
+        index[0] = Bond2index[4*ib+0];
+        index[1] = Bond2index[4*ib+1];
+        index[2] = Bond2index[4*ib+2];
+        index[3] = Bond2index[4*ib+3];
+
+        s[0] = Sigma[index[0]];
+        s[1] = Sigma[index[1]];
+        s[2] = Sigma[index[2]];
+        s[3] = Sigma[index[3]];
+
+        if(gsl_rng_uniform_pos(rng)<Prob[bits2int(s)]) {
+           merge(Ptree,Weight,index[0],index[1]); 
+           merge(Ptree,Weight,index[0],index[2]); 
+           merge(Ptree,Weight,index[0],index[3]); 
+        }
+
+        WeightOfConf += log(LocalWeight[bits2int(s)]);
+    }
+}
+
+void flip() {
+    for(int i=0;i<Nsite;i++) {
+        Flist[i]=0;
+        if(gsl_rng_uniform_pos(rng)<0.5) Flist[i]=1;
+    }
+
+    int r;
+    for(int i=0;i<Nsite;i++) {
+        r = root(Ptree,i);
+        if(Weight[r]>0){
+            Sigma[i] = (Sigma[i])^Flist[r];
+        }
+    }
+}
+
+int* Clusters;
+void analysis_cluster() {
+    if(Clusters==NULL) {
+        Clusters = (int*)malloc(sizeof(int)*Nsite);
+    }
+
+    int r;
+    int ncluster=0;
+    int msize=0;
+    for(int i=0;i<Nsite;i++) Clusters[i] = 0;
+    for(int i=0;i<Nsite;i++) {
+        r = root(Ptree,i);
+        if(!Clusters[r]) {
+            Clusters[r] = 1;
+            ncluster++;
+        } else {
+            Clusters[r]++;
+
+            if(Clusters[r]>msize) msize = Clusters[r];
+        }
+    }
+
+    printf("--------------------------\n");
+    printf("Nsite    = %d\n",Nsite);
+    printf("ncluster = %d\n",ncluster);
+    printf("msize    = %d\n",msize);
+}
+
+void show_state(int L, int T) {
+    for(int t=0;t<T;t++) {
+        for(int x=0;x<L;x++) {
+            printf("%d ",Sigma[x+t*L]);
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, char** argv) {
+    int L=atoi(argv[1]);
+    int T=atoi(argv[2]);
+    double mz = atof(argv[3]);
+    IAM = 2*mz/(1+mz);
+    Nfix = atoi(argv[4]);
+    double beta=atof(argv[5]);
+    int nblock  = atoi(argv[6]);
+    int nsample = atoi(argv[7]);
+    int nther   = atoi(argv[8]);
+    unsigned long int seed = atoi(argv[9]);
+    
+    T+=1;
+
+    Nsite = T*L;
+    Nbond = (T-1)*L;
+
+    Sigma = (int*)malloc(sizeof(int)*Nsite);
+    Bond2index = (int*)malloc(sizeof(int)*Nbond*2);
+    Ptree  = (int*)malloc(sizeof(int)*Nsite);
+    Weight = (int*)malloc(sizeof(int)*Nsite);
+    Flist  = (int*)malloc(sizeof(int)*Nsite);
+
+    rng = gsl_rng_alloc(gsl_rng_mt19937);
+    gsl_rng_set(rng,seed);
+
+    bond2index(L,T);
+    initial_state(L,T,3);
+
+    int b[4];
+    double w;
+    for(int i=0;i<16;i++) {
+        int2bits(i,b);
+        w = configuration2weight(i,beta);
+        LocalWeight[i] = w;
+        printf("%d (%d | %d %d %d) w=%.6e\n",bits2int(b),b[0],b[1],b[2],b[3],w);
+    }
+    for(int i=0;i<16;i++) {
+        Prob[i] = 1-LocalWeight[1]/LocalWeight[i];
+        printf("%.6e \n",Prob[i]);
+    }
+
+    for(int i=0;i<nther;i++) {
+        initial_tree();
+        boundary_condition(L,T,4);
+        clustering();
+        flip();
+        //analysis_cluster();
+    }
+
+    printf("staring the measurement...\n");
+    double* magz = (double*)malloc(sizeof(double)*T);
+
+    char magz_filename[128];
+    char weight_filename[128];
+    //sprintf(magz_filename,"magz_l_%d_mz_%.6f_beta_%.6f_t_%d.txt",L,mz,beta,T-1);
+    //sprintf(weight_filename,"weight_l_%d_mz_%.6f_beta_%.6f_t_%d.txt",L,mz,beta,T-1);
+    sprintf(magz_filename,"magz_l_%d_nfix_%d_beta_%.6f_t_%d.txt",L,Nfix,beta,T-1);
+    sprintf(weight_filename,"weight_l_%d_nfix_%d_beta_%.6f_t_%d.txt",L,Nfix,beta,T-1);
+    FILE* magz_file = fopen(magz_filename,"w");
+    FILE* weight_file = fopen(weight_filename,"w");
+
+    for(int k=0;k<nblock;k++) {
+        for(int i=0;i<T;i++) magz[i]=0;
+        for(int i=0;i<nsample;i++) {
+            for(int j=0;j<10;j++){
+                initial_tree();
+                boundary_condition(L,T,4);
+                clustering();
+                flip();
+            }
+
+            for(int t=0;t<T;t++) {
+                for(int x=0;x<L;x++) {
+                    magz[t]+=Sigma[x+L*t];
+                }
+            }
+            if(i%10==0)
+                fprintf(weight_file,"%16e ",WeightOfConf);
+        }
+        fprintf(weight_file,"\n");
+
+        for(int t=0;t<T;t++) {
+            magz[t] = magz[t]/nsample/L;
+            magz[t] = magz[t]*2-1;
+
+            fprintf(magz_file,"%.16e ",magz[t]);
+        }
+        fprintf(magz_file,"\n");
+    }
+
+    fclose(magz_file);
+    fclose(weight_file);
+
+    //for(int i=0;i<Nsite;i++) printf("%d %d\n",Ptree[i],Weight[i]);
+    //show_state(L,T);
+
+    return 0;
+}
---
+++ random_graph/conditional_sampling.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <gsl/gsl_rng.h>
+
+int* sigma;
+int* sigma_temp;
+
+int Nfix = 0;
+double* local_prob;
+double IAM=0;
+gsl_rng* rng;
+
+
+void set_local_prob(double beta){
+    if(local_prob==NULL) {
+        local_prob = (double*)malloc(sizeof(double)*4);
+    }
+    double p1,p0,z;
+    int ss;
+    for(int s=0;s<4;s++){
+        ss = s*2-3;
+        p1 = exp(beta*ss);
+        p0 = exp(-beta*ss);
+        z=p1+p0;
+        p1 = p1/z;
+        local_prob[s] = p1;
+    }
+}
+
+void initial_state(int type, int nsite) {
+    if(type==0){
+        for(int i=0;i<nsite;i++) {
+            sigma[i] = i%2;
+        }
+    } else if(type==1) {
+        for(int i=0;i<nsite;i++) {
+            sigma[i] = 1;
+        }
+    } else if(type==2) {
+        for(int i=0;i<nsite;i++) {
+            sigma[i] = 1;
+            if(i%4==0) sigma[i] = 0;
+        }
+    } else if(type==3) {
+        for(int i=0;i<nsite;i++) {
+            sigma[i] = 0;
+            if(gsl_rng_uniform_pos(rng)<IAM) {
+                sigma[i] = 1;
+            }
+        }
+    } else if(type==4) {
+        for(int i=0;i<nsite;i++) {
+            sigma[i] = 0;
+            if(gsl_rng_uniform_pos(rng)<IAM || i<Nfix) {
+                sigma[i] = 1;
+            }
+        }
+    }
+}
+
+int check_final_state(int nsite) {
+    int check=1;
+    for(int i=0;i<Nfix;i++) {
+        if(sigma[i]==0)
+            check=0;
+    }
+
+    return check;
+}
+
+double WeightOfConf;
+void update(int nsite) {
+    int s;
+    for(int i=0;i<nsite;i++) {
+        s = sigma[i]+sigma[(i+1)%nsite]+sigma[(i-1+nsite)%nsite];
+        if(gsl_rng_uniform_pos(rng)<local_prob[s]) {
+            sigma_temp[i]=1;
+            WeightOfConf += log(local_prob[s]);
+        } else { 
+            sigma_temp[i]=0;
+            WeightOfConf += log(1-local_prob[s]);
+        }
+    }
+
+    int* p = sigma;
+    sigma = sigma_temp;
+    sigma_temp = p;
+}
+
+void show_state(int nsite) {
+    for(int i=0;i<nsite;i++) {
+        printf("%d ",sigma[i]);
+    }
+    printf("\n");
+}
+
+int main(int argc, char** argv) {
+
+    int nsite = atoi(argv[1]);
+    int t = atoi(argv[2]);
+    //double mz = 0;
+    double mz = atof(argv[3]);
+    IAM = (mz+1)*0.5;
+    Nfix = atoi(argv[4]);
+    double beta = atof(argv[5]);
+    int nblock = atoi(argv[6]);
+    int nsample = atoi(argv[7]);
+    unsigned long int seed = atoi(argv[8]);
+
+    sigma      = (int*)malloc(sizeof(int)*nsite);
+    sigma_temp = (int*)malloc(sizeof(int)*nsite);
+
+    rng = gsl_rng_alloc(gsl_rng_mt19937);
+    gsl_rng_set(rng,seed);
+
+    set_local_prob(beta);
+
+    double* magz = (double*)malloc(sizeof(double)*t);
+    double* magz_temp = (double*)malloc(sizeof(double)*t);
+
+    char magz_filename[128];
+    char weight_filename[128];
+    //sprintf(magz_filename,"magz_l_%d_mz_%.6f_beta_%.6f_t_%d.txt",nsite,mz,beta,t);
+    //sprintf(weight_filename,"weight_l_%d_mz_%.6f_beta_%.6f_t_%d.txt",nsite,mz,beta,t);
+    sprintf(magz_filename,"magz_l_%d_nfix_%d_beta_%.6f_t_%d.txt",nsite,Nfix,beta,t);
+    sprintf(weight_filename,"weight_l_%d_nfix_%d_beta_%.6f_t_%d.txt",nsite,Nfix,beta,t);
+    FILE* magz_file = fopen(magz_filename,"w");
+    FILE* weight_file = fopen(weight_filename,"w");
+
+    for(int k=0;k<nblock;k++) {
+        for(int i=0;i<t;i++) magz[i]=0;
+        for(int j=0;j<nsample;j++) {
+            int check=0;
+            while(check==0){
+                WeightOfConf = 0;
+                initial_state(3,nsite);
+                for(int i=0;i<t;i++) {
+                    //show_state(nsite);
+                    update(nsite);
+                    magz_temp[i]=0;
+                    for(int i_site=0;i_site<nsite;i_site++) {
+                        magz_temp[i] += sigma[i_site];
+                    }
+                }
+                check = check_final_state(nsite);
+            }
+
+            for(int i=0;i<t;i++) 
+                magz[i]+=magz_temp[i];
+
+            if(j%10==0)
+                fprintf(weight_file,"%16e ",WeightOfConf);
+        }
+        fprintf(weight_file,"\n");
+
+        for(int i=0;i<t;i++) {
+            magz[i] = magz[i]/nsample/nsite;
+            magz[i] = magz[i]*2-1;
+
+            fprintf(magz_file,"%.16e ",magz[i]);
+        }
+        fprintf(magz_file,"\n");
+
+    }
+
+    fclose(magz_file);
+    fclose(weight_file);
+
+    return 0;
+}
